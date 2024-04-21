06e35b3b95a8afaaa370b78fddf74b8544905403
bhavesh0009 <bhavesh.ghodasara@gmail.com>
2024-04-21 09:30:11 +0530

    review post added.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -29,9 +30,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const validateCampground = (req, res, next) => {
-
     const { error } = campgroundSchema.validate(req.body);
-    // console.log(result);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400);
@@ -78,12 +88,19 @@ app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
+app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res) => {
+    // res.send("YOU MADE IT!!!!");
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+app.post("/campgrounds/:id/reviews", validateCampground, catchAsync(async (req, res) => {
+
+}))
 
 
 app.all("*", (req, res, next) => {
