225058724fca330f54d1144b2cbb2a31cd014a8a
bhavesh0009 <bhavesh.ghodasara@gmail.com>
2024-05-02 19:39:06 +0530

    Added register basic form.

--- routes/users.js
+++ routes/users.js
@@ -5,3 +5,10 @@ const User = require('../models/user');
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+router.post('/register', (req, res) => {
+    res.send(req.body);
+});
+
+
+module.exports = router;
