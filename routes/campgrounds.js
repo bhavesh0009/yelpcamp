767ef6aa32bd9536b00bac70cc99140b30ac7d25
bhavesh0009 <bhavesh.ghodasara@gmail.com>
2024-05-12 14:28:23 +0530

    Added roter.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,23 +2,20 @@ const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
+// const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
-const ObjectID = require('mongoose').Types.ObjectId;
-
-router.get("/", catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(validateCampground, isLoggedIn, isAuthor, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put("/:id", validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete("/:id", isLoggedIn, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router
