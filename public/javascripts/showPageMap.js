cd55a551853f4ea3f0b167e8db22cbb0af7706c4
bhavesh0009 <bhavesh.ghodasara@gmail.com>
2024-05-31 20:58:07 +0530

    cenetering the map on campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,14 @@
 mapboxgl.accessToken = mapToken;
+// campground = JSON.parse(campground);
+console.log(campground.geometry)
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates,
     zoom: 9, // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
--- seeds/index.js
+++ seeds/index.js
@@ -19,24 +19,6 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-
-// const seedDB = async () => {
-//     await Campground.deleteMany({});
-//     for (let i = 0; i < 50; i++) {
-//         const random1000 = Math.floor(Math.random() * 1000);
-//         const camp = new Campground({
-//             author: '663720abd3d8f973432449ab',
-//             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-//             title: `${sample(descriptors)} ${sample(places)}`,
-//             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit delectus!',
-//             price: Math.floor(Math.random() * 20) + 10,
-//             images: [
-
-//             ]
-//         })
-//         await camp.save();
-//     }
-// }
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
@@ -48,7 +30,15 @@ const seedDB = async () => {
             title: campDetails.campDetails[randomCampDetail].title,
             description: campDetails.campDetails[randomCampDetail].description,
             price: Math.floor(Math.random() * 20) + 10,
-            images: campDetails.campDetails[randomCampDetail].images
+            images: campDetails.campDetails[randomCampDetail].images,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+
         })
         await camp.save();
     }
